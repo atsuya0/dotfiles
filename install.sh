f2beaa647debe431aba173cb1fbeab31b606453d
tayusa <37957375+tayusa@users.noreply.github.com>
2019-03-20 00:37:13 +0900

    update

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 set -euCo pipefail
 
 function fetch_dotfiles() {
-  curl -fsSLO https://github.com/tayusa/dotfiles/archive/master.zip
-  bsdtar xf master.zip
-  rm master.zip
-  mv dotfiles-mater "${DOTFILES}"
+  curl -fsSLO https://github.com/tayusa/dotfiles/archive/master.tar.gz
+  tar -xzf master.tar.gz
+  rm master.tar.gz
+  mv dotfiles-master "${DOTFILES}"
 }
 
 function main() {
