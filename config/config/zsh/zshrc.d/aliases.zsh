e49470efc27e05a7f8cc0483654d002e5953ba36
tayusa <37957375+tayusa@users.noreply.github.com>
2020-12-06 23:50:40 +0900

    pip3を指定してライブラリをアップグレード

--- config/config/zsh/zshrc.d/aliases.zsh
+++ config/config/zsh/zshrc.d/aliases.zsh
@@ -32,6 +32,7 @@ alias lock='i3lock'
 alias noise='paplay /usr/share/sounds/alsa/Noise.wav'
 alias wallpaper='feh --no-fehbg --bg-scale /home/tayusa/contents/pictures/wallpaper/arch/Ju5PuBC.jpg'
 alias pip-upgrade="pip list -o | sed '1,2d' | tr -s ' ' | cut -d' ' -f1 | xargs -I{} pip install -U {}"
+alias pip3-upgrade="pip3 list -o | sed '1,2d' | tr -s ' ' | cut -d' ' -f1 | xargs -I{} pip3 install -U {}"
 
 alias -g @g='| grep'
 alias -g @l='| less'
