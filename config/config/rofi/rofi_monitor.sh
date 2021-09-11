8e82869b37a53ea7738395805b031c3209ba7309
atsuya0 <37957375+atsuya0@users.noreply.github.com>
2021-09-11 22:22:59 +0900

    名前ミス

--- config/config/rofi/rofi_monitor.sh
+++ config/config/rofi/rofi_monitor.sh
@@ -10,7 +10,7 @@ function main() {
   secondary=$(xrandr | grep ' connected' | cut -d' ' -f1 | grep -v ${primary})
 
   [[ $# -ne 0 ]] \
-    && xrandr --output ${second} --left-of ${primary} --mode $1 \
+    && xrandr --output ${secondary} --left-of ${primary} --mode $1 \
     || xrandr \
         | sed -n "/^HDMI1/,/^[^ ]/p" \
         | grep -o '[[:digit:]]\+x[[:digit:]]\+\(i\|[[:blank:]]\)' \
