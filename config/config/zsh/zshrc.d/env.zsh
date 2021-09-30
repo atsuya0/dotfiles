455d439ca3b557b492e7dbf0f77400b57560ff79
atsuya0 <37957375+atsuya0users.noreply.github.com>
2021-10-01 00:58:09 +0900

    vscodeのpathを通す

--- config/config/zsh/zshrc.d/env.zsh
+++ config/config/zsh/zshrc.d/env.zsh
@@ -11,28 +11,32 @@ typeset -a path=(
       && echo "$(ruby -e 'print Gem.user_dir')/bin")
 )
 
-case ${OSTYPE} in
-  darwin* )
-    export TRASH_CAN_PATH="${HOME}/Trash"
-    typeset -ar path=(
-      ${path}
-      /usr/local/opt/coreutils/libexec/gnubin
-      /usr/local/opt/findutils/libexec/gnubin
-      /usr/local/opt/gnu-sed/libexec/gnubin
-      /usr/local/bin
-      /usr/bin
-      /bin
-      /usr/sbin
-      /sbin
-      /usr/local/opt/mysql@5.6/bin
-      "${HOME}/google-cloud-sdk/bin"
-    )
-  ;;
-  'linux-gnu' )
-    export TRASH_CAN_PATH="${HOME}/.Trash"
-    typeset -ar path=(
-      ${path}
-      /usr/bin
-    )
-  ;;
-esac
+if [[ ${OSTYPE} =~ 'darwin' ]]; then
+  export TRASH_CAN_PATH="${HOME}/Trash"
+  typeset -ar path=(
+    ${path}
+    /usr/local/opt/coreutils/libexec/gnubin
+    /usr/local/opt/findutils/libexec/gnubin
+    /usr/local/opt/gnu-sed/libexec/gnubin
+    /usr/local/bin
+    /usr/bin
+    /bin
+    /usr/sbin
+    /sbin
+    /usr/local/opt/mysql@5.6/bin
+    "${HOME}/google-cloud-sdk/bin"
+  )
+elif [[ -n ${WSLENV} ]]; then
+  export TRASH_CAN_PATH="${HOME}/.Trash"
+  typeset -ar path=(
+    ${path}
+    /usr/bin
+    '/mnt/c/Users/atsuy/AppData/Local/Programs/Microsoft VS Code/bin'
+  )
+elif [[ ${OSTYPE} == 'linux-gnu' ]]; then
+  export TRASH_CAN_PATH="${HOME}/.Trash"
+  typeset -ar path=(
+    ${path}
+    /usr/bin
+  )
+fi
