255f1ef656e8e99168f38ddc9b3228246d265f1e
atsuya0 <37957375+atsuya0@users.noreply.github.com>
2021-10-02 13:32:44 +0900

    ディレクトリ構成を変更

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,20 +3,17 @@
 set -euCo pipefail
 
 function link_config_file() {
-  local -r config="${DOTFILES:-${HOME}/dotfiles}/config"
+  local -r dotfiles="${DOTFILES:-${HOME}/dotfiles}"
 
-  ls -A "${config}/home" | while read -r file; do
-    ln -sf "${config}/home/${file}" ${HOME}/
+  ls -A "${dotfiles}/home" | while read -r file; do
+    [[ -f "${HOME}/${file}" ]] && rm -r "${HOME}/${file}"
+    ln -s "${dotfiles}/home/${file}" ${HOME}/
   done
 
-  ls "${config}/config" | while read -r file; do
-    rm -r "${XDG_CONFIG_HOME:-${HOME}/.config}/${file}"
-    ln -sf "${config}/config/${file}" "${XDG_CONFIG_HOME:-${HOME}/.config}/"
-  done
-
-  ls "${config}/cache" | while read -r file; do
-    rm -r "${XDG_CACHE_HOME:-${HOME}/.cache}/${file}"
-    ln -sf "${config}/cache/${file}" "${XDG_CACHE_HOME:-${HOME}/.cache}/"
+  ls "${dotfiles}/config" | while read -r file; do
+    local target="${XDG_CONFIG_HOME:-${HOME}/.config}/${file}"
+    [[ -f ${target} ]] && rm -r ${target}
+    ln -s "${dotfiles}/config/${file}" ${target}
   done
 }
 
@@ -31,7 +28,7 @@ function fetch_surround-nvim() {
 
 function main() {
   link_config_file
-  fetch_surround-nvim
+  # fetch_surround-nvim
 }
 
 main $@
