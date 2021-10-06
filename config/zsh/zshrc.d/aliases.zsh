160801ca18363075313a1edf60782db91229ab33
atsuya0 <37957375+atsuya0users.noreply.github.com>
2021-10-06 23:32:35 +0900

    alias

--- config/zsh/zshrc.d/aliases.zsh
+++ config/zsh/zshrc.d/aliases.zsh
@@ -21,6 +21,7 @@ alias nano='nano -$ -l -i -O -m -c' # '-$liOmc' does not work.
 
 alias tree='tree -C'
 alias dc='docker-compose'
+alias k='kubectl'
 alias poweroff='confirm systemctl poweroff'
 alias reboot='confirm systemctl reboot'
 alias logout='confirm i3-msg exit'
@@ -31,7 +32,7 @@ alias lock='i3lock'
 alias noise='paplay /usr/share/sounds/alsa/Noise.wav'
 alias wallpaper='feh --no-fehbg --bg-scale /home/tayusa/contents/pictures/wallpaper/arch/Ju5PuBC.jpg'
 alias pip-upgrade="pip list -o | sed '1,2d' | tr -s ' ' | cut -d' ' -f1 | xargs -I{} pip install -U {}"
-alias ss='gnome-screenshot --area'
+alias gnomess='gnome-screenshot --area'
 alias pip3-upgrade="pip3 list -o | sed '1,2d' | tr -s ' ' | cut -d' ' -f1 | xargs -I{} pip3 install -U {}"
 
 alias -g @g='| grep'
