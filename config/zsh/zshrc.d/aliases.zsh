ee7551e6e8f1f45a85e705057c6ce644967c593a
atsuya0 <37957375+atsuya0@users.noreply.github.com>
2021-12-21 00:29:57 +0900

    古くなったスクリプトを一掃

--- config/zsh/zshrc.d/aliases.zsh
+++ config/zsh/zshrc.d/aliases.zsh
@@ -30,7 +30,7 @@ alias xephyr='Xephyr -wr -resizeable :1'
 # alias lock='light-locker-command -l'
 alias lock='i3lock'
 alias noise='paplay /usr/share/sounds/alsa/Noise.wav'
-alias wallpaper='feh --no-fehbg --bg-scale /home/tayusa/contents/pictures/wallpaper/arch/Ju5PuBC.jpg'
+alias wallpaper="feh --no-fehbg --bg-scale ${HOME}/contents/pictures/wallpaper/arch/Ju5PuBC.jpg"
 alias pip-upgrade="pip list -o | sed '1,2d' | tr -s ' ' | cut -d' ' -f1 | xargs -I{} pip install -U {}"
 alias pip3-upgrade="pip3 list -o | sed '1,2d' | tr -s ' ' | cut -d' ' -f1 | xargs -I{} pip3 install -U {}"
 
