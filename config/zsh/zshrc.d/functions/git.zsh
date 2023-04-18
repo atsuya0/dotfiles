e8f6d2d73cbec577a31a4d77c73a4cbb7168ee81
atsuya0 <37957375+atsuya0users.noreply.github.com>
2023-04-18 22:46:01 +0900

    gitlab系の関数も入れる

--- config/zsh/zshrc.d/functions/git.zsh
+++ config/zsh/zshrc.d/functions/git.zsh
@@ -152,6 +152,55 @@ function print_updated_prs() {
   done
 }
 
+
+function gmr() {
+  is_managed_by_git || return 1
+  [[ -z ${commands[jq]} ]] && { echo 'jq is required'; return 1; }
+
+  local -r url=$(curl --header "PRIVATE-TOKEN: ${GITLAB_ACCESS_TOKEN}" \
+    "https://${GITLAB_FQDN}/api/v4/projects/$(git config --get remote.origin.url | cut -d/ -f 4- | sed 's@/@%2F@g;s/.git$//')/merge_requests?source_branch=$(git branch --show-current)" | jq -r '.[0].web_url')
+
+  if [[ ${OSTYPE} =~ 'darwin' ]]; then
+    open ${url}
+  elif [[ -n ${WSL_INTEROP} ]]; then
+    '/mnt/c/Program Files/Google/Chrome/Application/chrome.exe' ${url}
+  else
+    xdg-open ${url}
+  fi
+}
+
+function git_clone_to_path() {
+  local repo_path=$(sed 's/[^:]*:\/\/[^\/]*\/\([^\.]*\)\.git/\1/' <<< $1 | xargs dirname)
+  mkdir -p ${PWD}/${repo_path}
+  (
+    cd ${PWD}/${repo_path}
+    [[ -d $(echo $1 | xargs basename | cut -d. -f1) ]] || git clone $1
+  )
+}
+
+function gitlab_projects_total_pages_by_group_id() {
+  [[ $# -eq 0 ]] && return 1
+
+  local -r total=$(curl -i -s --header "PRIVATE-TOKEN: ${GITLAB_ACCESS_TOKEN}" \
+    "https://${GITLAB_FQDN}/api/v4/groups/$1/projects?include_subgroups=true&simple=true&per_page=1" \
+    | grep x-total: | cut -d' ' -f2 | sed 's/[[:space:]]//')
+  expr ${total} / 100 + 1
+}
+
+function recursive_git_clone_to_path_by_group_id() {
+  [[ -z ${commands[jq]} ]] \
+    && { echo 'jq is required'; return 1; }
+  [[ $# -eq 0 ]] && return 1
+
+  for i in $(seq $(gitlab_projects_total_pages_by_group_id $1)); do
+    for repo in $(curl -s --header "PRIVATE-TOKEN: ${GITLAB_ACCESS_TOKEN}" \
+      "https://${GITLAB_FQDN}/api/v4/groups/$1/projects?include_subgroups=true&simple=true&per_page=100&page=$i" \
+      | jq -r '.[].http_url_to_repo'); do
+      git_clone_to_path $repo
+    done
+  done
+}
+
 alias gs='git status'
 alias ga='git add'
 alias gcm='git commit -m'
@@ -164,6 +213,8 @@ alias gd='git diff --histogram'
 alias gdc='git diff --histogram --cached'
 alias gc1='git clone -b master --depth 1'
 alias gcp='git cherry-pick'
+alias gc='git_clone_to_path'
+alias rgc='recursive_git_clone_to_path_by_group_id'
 
 alias -g @gb='$(__git_branch_list__)'
 alias -g @gw='$(__git_working_tree_status__)'
