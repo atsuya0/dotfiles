aacab023defd9ea0083ec23c69e55636dbcc2768
atsuya0 <37957375+atsuya0@users.noreply.github.com>
2023-05-02 01:09:00 +0900

    astronvim insert mode抜けるときにIMEをOFFにする

--- config/astronvim/lua/user/init.lua
+++ config/astronvim/lua/user/init.lua
@@ -1,3 +1,16 @@
 return {
   --colorscheme = "everforest"
+
+  vim.api.nvim_create_augroup("imeoff", { clear = true }),
+  vim.api.nvim_create_autocmd("InsertLeave", {
+    desc = "Turn off IME when exiting from insert mode",
+    pattern = "*",
+    group = "imeoff",
+    callback = function()
+      if vim.fn.has("unix") == 1 then
+        vim.fn.system('fcitx5-remote -c')
+      elseif vim.fn.has("macunix") == 1 then
+      end
+    end,
+  })
 }
