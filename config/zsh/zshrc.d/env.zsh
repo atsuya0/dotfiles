2877026b3c85f8b34fe3e9adb85e53dbc2c18d00
atsuya0 <37957375+atsuya0users.noreply.github.com>
2024-04-20 22:55:33 +0900

    limaのinstanceの有無を確認する

--- config/zsh/zshrc.d/env.zsh
+++ config/zsh/zshrc.d/env.zsh
@@ -54,7 +54,7 @@ if [[ ${OSTYPE} =~ 'darwin' ]]; then
     "${HOME}/google-cloud-sdk/bin"
   )
 
-  [[ -n ${commands[limactl]} ]] && export DOCKER_HOST=$(limactl list docker --format 'unix://{{.Dir}}/sock/docker.sock')
+  [[ $(limactl list -q 2> /dev/null | head -1) == 'default' ]] && export DOCKER_HOST=$(limactl list default --format 'unix://{{.Dir}}/sock/docker.sock')
   #[[ -n ${commands[podman]} ]] && export DOCKER_HOST="unix://$(podman machine inspect $(podman machine info -f '{{.Host.CurrentMachine}}') --format '{{.ConnectionInfo.PodmanSocket.Path}}')"
   #export KIND_EXPERIMENTAL_PROVIDER=podman
 elif [[ -n ${WSL_INTEROP} ]]; then
