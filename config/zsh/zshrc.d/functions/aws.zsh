006c492f5042807d9de35c67e508f8ce708a0bee
atsuya0 <37957375+atsuya0users.noreply.github.com>
2024-09-25 12:31:34 +0900

    コマンドでログインできなくなった

--- config/zsh/zshrc.d/functions/aws.zsh
+++ config/zsh/zshrc.d/functions/aws.zsh
@@ -1,8 +1,6 @@
 function set_aws_profile() {
   local -r profile=$(aws configure list-profiles | fzf)
   [[ -z ${profile} ]] && return
-  [[ -n $(aws configure get sso_account_id --profile ${profile}) ]] \
-    && aws sso login --profile ${profile}
   export AWS_PROFILE=${profile}
 }
 
