6cd9a373b3591163a019c8164212a1df37de752d
atsuya0 <37957375+atsuya0users.noreply.github.com>
2025-03-18 03:58:07 +0900

    NvChadのデフォルトのキーバインドを削除

--- config/NvChad/lua/mappings.lua
+++ config/NvChad/lua/mappings.lua
@@ -4,11 +4,6 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
-
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
-
 map("n", "j", "gj")
 map("n", "k", "gk")
 -- Emacs key bindings.
