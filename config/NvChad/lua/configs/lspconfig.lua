8c71355b7a8ca5662ab81a5cf3df0022060b612f
atsuya0 <37957375+atsuya0users.noreply.github.com>
2025-03-18 21:16:19 +0900

    シンタックスハイライト&フォーマット

--- config/NvChad/lua/configs/lspconfig.lua
+++ config/NvChad/lua/configs/lspconfig.lua
@@ -3,11 +3,10 @@ require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
 
--- EXAMPLE
-local servers = { "html", "cssls" }
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
+local servers = { "gopls", "terraformls", "bashls", "jsonls", "yamlls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
--- lsps with default config
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = nvlsp.on_attach,
@@ -15,10 +14,3 @@ for _, lsp in ipairs(servers) do
     capabilities = nvlsp.capabilities,
   }
 end
-
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
--- config/NvChad/lua/plugins/init.lua
+++ config/NvChad/lua/plugins/init.lua
@@ -1,8 +1,22 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
+    event = "BufWritePre", -- uncomment for format on save
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "black", "isort" },
+        go = { "gofmt", "goimports" },
+        sql = { "sqlfmt" },
+        terraform = { "terraform_fmt" },
+        ansible = { "ansible-lint" },
+        bash = { "shfmt" },
+      },
+      format_on_save = true,
+      format_options = {
+        timeout_ms = 1000,
+      },
+    },
   },
 
   -- These are some examples, uncomment them if you want to see them work!
@@ -13,13 +27,22 @@ return {
     end,
   },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "markdown",
+        "bash",
+        "go",
+        "python",
+        "terraform",
+        "hcl",
+      },
+    },
+  },
 }
