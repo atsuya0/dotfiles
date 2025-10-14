69208c2d99253e8f79f2f79fd334d0f4b11bf39b
atsuya0 <37957375+atsuya0users.noreply.github.com>
2025-10-14 20:25:39 +0900

    fix

--- bin/images_viewer.sh
+++ bin/images_viewer.sh
@@ -2,6 +2,19 @@
 
 set -eCo pipefail
 
+function initialize() {
+  tput smcup
+  tput civis
+  stty -echo cbreak
+}
+
+function cleanup() {
+  tput rmcup
+  tput cnorm
+  tput sgr0
+  stty sane
+}
+
 function main() {
   [[ -z ${commands[chafa]} ]] && { echo 'chafa is required'; return 1; }
   [[ -z ${commands[eza]} ]] && { echo 'eza is required'; return 1; }
@@ -14,8 +27,14 @@ function main() {
 
   [[ ${#files} == 0 ]] && return 1
 
+  local -a fav_list
+  [[ -f ./favorites.txt ]] && fav_list=($(cat ./favorites.txt))
+
   while true; do
-    chafa --clear -f sixel --scale max --align mid,mid "${files[${current_index}]}"
+    # 予期せず標準入力を消費するので/dev/nullに接続する
+    chafa --clear -f sixel --scale max --align mid,mid "${files[${current_index}]}" < /dev/null
+    echo "${current_index} / ${#files}"
+    echo ${fav_list}
     read -k 1 input
     case ${input} in
       ' ' )
@@ -27,14 +46,46 @@ function main() {
       'k' )
         [[ ${current_index} -ne 1 ]] && (( current_index-- ))
       ;;
-      'q' )
-        break
+      'l' )
+        [[ ${current_index} -le (( ${#files} - 5 )) ]] && (( current_index += 5 ))
       ;;
       'h' )
+        [[ ${current_index} -gt 5 ]] && (( current_index -= 5 ))
+      ;;
+      'f' )
+        fav_list+=(${current_index})
+        fav_list=(${(nu)fav_list})
+      ;;
+      'F' )
+        fav_list=(${fav_list:#${current_index}})
+      ;;
+      'J' )
+        local fav_index
+        for fav_index in ${fav_list}; do
+          [[ ${current_index} -lt ${fav_index} ]] \
+            && [[ ${current_index} -ne ${#files} ]] \
+            && current_index=${fav_index} \
+            && break
+        done
+      ;;
+      'K' )
+        local fav_index
+        for fav_index in ${(On)fav_list}; do
+          [[ ${current_index} -gt ${fav_index} ]] \
+            && [[ ${current_index} -ne 1 ]] \
+            && current_index=${fav_index} \
+            && break
+        done
+      ;;
+      'q' )
         break
       ;;
     esac
   done
+
+  [[ ${#fav_list} == 0 ]] || echo ${fav_list[@]} >! ./favorites.txt
 }
 
+trap cleanup EXIT INT TERM
+initialize
 main $1
