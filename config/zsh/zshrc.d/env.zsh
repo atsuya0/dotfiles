b6552f1d41d272ed0a8fe74b0cbab6066126d69a
atsuya0 <37957375+atsuya0users.noreply.github.com>
2026-02-27 14:25:03 +0900

    remove unused env

--- config/zsh/zshrc.d/env.zsh
+++ config/zsh/zshrc.d/env.zsh
@@ -1,8 +1,4 @@
-export TF_LOG=trace
-export TF_LOG_PATH="${HOME}/.terraform.log"
 export PYENV_ROOT="${HOME}/.pyenv"
-export NNN_PLUG='p:preview-tui;z:!trs move "$nnn"*'
-export NNN_OPENER="${DOTFILES}/bin/nnn_opener.sh"
 export FZF_DEFAULT_OPTS="-m --height=80% --reverse --exit-0 --bind 'ctrl-y:execute-silent(echo {} | cut -f2- | pbcopy)+abort'" # <C-y>で文字列をコピー
 export FZF_CTRL_R_OPTS="--preview='echo {}' --preview-window=down:3:hidden:wrap --bind 'ctrl-v:toggle-preview'" # <C-v>で見切れたコマンドを表示
 
